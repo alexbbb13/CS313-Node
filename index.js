38a74de6a2ece73b712bccf295ff8fe577595dd0
alexbbb13 <alex_bbb@mail.ru>
2019-06-22 15:16:03 +0300

    Heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/getrate', (req, res) => res.render('pages/result', prepareData(req)))
+  .post('/getrate', (req, res) => res.render('pages/result', prepareData(req)))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 function prepareData(req){
